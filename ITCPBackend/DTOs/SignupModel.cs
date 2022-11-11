c9096b530382d0a936f361c2d0890ccfd17bb021
Natiq Butt <natiqbutt2018@gmail.com>
2022-11-11 21:58:47 +0500

    DTOs > added for Signup

---
+++ ITCPBackend/DTOs/SignupModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ITCPBackend.DTOs
+{
+    public class SignupModel
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int status { get; set; }
+    }
+}
