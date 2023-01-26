5198de5d31a832f7f1e37418e8df8261320bb01a
Natiq Butt <natiqbutt2018@gmail.com>
2023-01-27 00:36:18 +0500

    CompeteProjectDto

---
+++ ITCPBackend/DTOs/CompeteProjectDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ITCPBackend.DTOs
+{
+    public class CompeteProjectDto
+    {
+        public int? Id { get; set; }
+        public string? MDA { get; set; }
+        public string? BudgetCode { get; set; }
+        public int? MDASector { get; set; }
+        public string? ProjectName { get; set; }
+        public string? ProjectObjectives { get; set; }
+        public string? ProjectDescription { get; set; }
+        public string? ProjectClassification { get; set; }
+        public string? accesstoken { get; set; }
+        public int? ProjectId { get; set; }
+        public string? DurationType { get; set; }
+        public string? FirstStartDate { get; set; }
+        public string? FirstEndDate { get; set; }
+        public string? SecondStartDate { get; set; }
+        public string? SecondEndDate { get; set; }
+        public string? ThirdStartDate { get; set; }
+        public string? ThirdEndDate { get; set; }
+    }
+}
