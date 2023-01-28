60ed07ba6e8e8fbd093d763772355192d69772bf
Natiq Butt <natiqbutt2018@gmail.com>
2023-01-29 01:36:39 +0500

    Edit Get call for data complete

---
+++ ITCPBackend/Migrations/20230128171229_durationmodel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITCPBackend.Migrations
+{
+    public partial class durationmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "project_durations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstEndDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstStartDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondEndDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondStartDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThirdEndDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThirdStartDate",
+                table: "project_durations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstEndDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "FirstStartDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "SecondEndDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "SecondStartDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "ThirdEndDate",
+                table: "project_durations");
+
+            migrationBuilder.DropColumn(
+                name: "ThirdStartDate",
+                table: "project_durations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "project_durations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "project_durations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
