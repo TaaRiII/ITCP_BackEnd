e6fa1799f3790491651b7c312d4d2eea5e3800f6
Natiq Butt <natiqbutt2018@gmail.com>
2023-02-21 22:44:27 +0500

    Reomove chart api from this controllar

--- ITCPBackend/Controllers/UsersController.cs
+++ ITCPBackend/Controllers/UsersController.cs
@@ -386,28 +386,5 @@ namespace ITCPBackend.Controllers
             return Ok(EntryList);
         }
         #endregion
-        #region Dashboard Graphies data
-        [HttpGet]
-        public IEnumerable<ChartData> GetProjectChartData()
-        {
-            var all = _dbcontext.projects.Where(m => m.Status != (int)Constants.ProjectStatus.Draft && m.Status != (int)Constants.ProjectStatus.Submit && m.Status != (int)Constants.ProjectStatus.MDAReject).Count();
-            var submit = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.Submit).Count();
-            var draft = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.Draft).Count();
-            var sectRejected = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.SectReject).Count();
-            var mdaRejected = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.MDAReject).Count();
-            var mdaApprove = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.MDApprove).Count();
-            var sectApprove = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.SectApprove).Count();
-            var commitee = _dbcontext.projects.Where(m => m.Status == (int)Constants.ProjectStatus.Comeetee).Count();
-            var data = new List<ChartData>
-            {
-                new ChartData { Region = "All", Value = all },
-                new ChartData { Region = "New Project", Value = mdaApprove },
-                new ChartData { Region = "Approved Secretariat", Value = sectApprove },
-                new ChartData { Region = "Reject Secretariat", Value = sectRejected },
-                new ChartData { Region = "Commitee", Value = commitee },
-            };
-            return data;
-        }
-        #endregion
     }
 }
